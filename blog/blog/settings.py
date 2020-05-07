cbeb8dc9e1106d9f24e81dc06ae3282341543383
Jakub <nartowiczjakub@gmail.com>
2020-05-07 20:05:16 +0200

    Remove manager app

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'post',
-    'manager',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('post.urls')),
-    path('admin/', admin.site.urls),
-    path('manager/', include('manager.urls'))
+    path('admin/', admin.site.urls)
 ]
