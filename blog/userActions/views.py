32e098d2c4d3ec17078a6b29103177463ce79e84
Jakub <nartowiczjakub@gmail.com>
2020-05-08 19:25:25 +0200

    Fix spaces

--- blog/userActions/views.py
+++ blog/userActions/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
 # Create your views here.
 
 def login(request):
     return render(request, 'userActions/login.html')
+
 
 def register(request):
     return render(request, 'userActions/register.html')
