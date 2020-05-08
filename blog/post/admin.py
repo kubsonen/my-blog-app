fad261267f4f15aa2c221509ebb1025baaf7ade0
Michal Jezierski <jezierski.michal13@gmail.com>
2020-05-08 20:14:27 +0200

    Dodanie formularzy do każdego modelu w panelu administratora

--- blog/post/admin.py
+++ blog/post/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Post
+from .models import Post 
+from .models import Images 
+from .models import Comment 
+from .models import Tag 
+from .models import Category 
+from .models import Like 
+
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Images)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Like)
