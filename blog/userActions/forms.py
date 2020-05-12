369b785cc6e8a75bf7c601516b2e941240e15e55
Michal Jezierski <jezierski.michal13@gmail.com>
2020-05-12 02:19:55 +0200

    Dodanie sprawdzanie przy rejestracji czy podany adres email jest unikalny

--- blog/userActions/forms.py
+++ blog/userActions/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 class LoginForm(forms.Form):
     login = forms.CharField(label="login")
@@ -12,10 +13,19 @@ class RegisterForm(UserCreationForm):
     email = forms.CharField(widget=forms.EmailInput(attrs={'class':'input',  'placeholder':'E-mail'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'input', 'placeholder':'Password' }, render_value=True))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'input', 'placeholder':'Repeat password'}, render_value=True))
+
+
     class Meta:
         model=User
         fields = ('username', 'email', 'password1', 'password2')
         widgets = {
             'username': forms.TextInput(attrs={'class':'input', 'placeholder':'Login'}),
         }
+    
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email is not unique")
+        
 
